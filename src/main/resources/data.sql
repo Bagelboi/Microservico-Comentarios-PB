1da29ee8aab3f73538fdb5a3eb93835d245f6823
Bagelboi <donkeysoda22@gmail.com>
2024-09-09 03:30:10 -0300

    oloco

--- src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO TB_SECAO (id, id_projeto) values ('1', '111');
-INSERT INTO TB_COMENTARIO(id, nome, conteudo, secao_id) values('1', 'Daniel', 'Teste.', '1');
-INSERT INTO TB_COMENTARIO(id, nome, conteudo, secao_id) values('2', 'Antonio', 'Teste 2.', '1');
